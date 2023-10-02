31fa85667eba76e20b248b91883f3d66fbe486e6
Pedro França <jppfeitosa@gmail.com>
2023-10-02 16:10:48 -0300

    fix: banner layout

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -61,14 +61,14 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
           fetchPriority={lcp ? "high" : "auto"}
           src={mobile}
           width={360}
-          height={600}
+          height={420}
         />
         <Source
           media="(min-width: 768px)"
           fetchPriority={lcp ? "high" : "auto"}
           src={desktop}
           width={1440}
-          height={600}
+          height={420}
         />
         <img
           class="object-cover w-full h-full"
